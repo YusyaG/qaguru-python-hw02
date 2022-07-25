c870cec5f6c12b0dd051e60391b0284a18bff1ed
YusyaG <gunchenko.yui@gmail.com>
2022-07-25 22:30:22 +0300

    Add homework_02_corrected

--- test_google_selene.py
+++ test_google_selene.py
@@ -7,4 +7,4 @@ def test_browser_successful(open_browser):
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 def test_browser_failed(open_browser):
-    browser.element('[id="search"]').should(have.text('абракадабра'))
+    browser.element('[id="search"]').should(have.no.text('абракадабра'))
